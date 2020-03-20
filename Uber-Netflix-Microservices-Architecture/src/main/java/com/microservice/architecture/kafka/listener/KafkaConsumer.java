a8c36c23e2087b7424d4340c942d01e1c76779f6
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 22:55:28 +0530

    KafkaConsumer config

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/listener/KafkaConsumer.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/listener/KafkaConsumer.java
@@ -3,13 +3,21 @@ package com.microservice.architecture.kafka.listener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.microservice.architecture.entity.MS;
+
 @Service
 public class KafkaConsumer {
 
 	
 	@KafkaListener(topics="Kafka1",groupId="string")
-	public void consume(String msg)
+	public void consumeString(String msg)
 	{
 		System.out.println("Consumed message "+msg);
+	}
+	
+	@KafkaListener(topics="Kafka2",groupId="json")
+	public void consumeMS(MS ms)
+	{
+		System.out.println("Consumed message "+ms);
 	}
 }
