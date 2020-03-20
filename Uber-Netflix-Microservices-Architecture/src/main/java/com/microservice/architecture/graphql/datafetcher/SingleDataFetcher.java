04b81a715e669ff4a82153806fd3b218d5f39b65
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 21:05:20 +0530

    DF MS

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/graphql/datafetcher/SingleDataFetcher.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/graphql/datafetcher/SingleDataFetcher.java
@@ -3,7 +3,7 @@ package com.microservice.architecture.graphql.datafetcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.microservice.architecture.entity.Microservices;
+import com.microservice.architecture.entity.MS;
 import com.microservice.architecture.repository.MicroservicesRepository;
 
 import graphql.schema.DataFetcher;
@@ -16,7 +16,7 @@ public class SingleDataFetcher implements DataFetcher {
 	MicroservicesRepository microserviceRepository;
 
 	@Override
-	public Microservices get(DataFetchingEnvironment environment) {
+	public MS get(DataFetchingEnvironment environment) {
 		
 		String key= environment.getArgument("id");
 		int i=Integer.parseInt(key);
