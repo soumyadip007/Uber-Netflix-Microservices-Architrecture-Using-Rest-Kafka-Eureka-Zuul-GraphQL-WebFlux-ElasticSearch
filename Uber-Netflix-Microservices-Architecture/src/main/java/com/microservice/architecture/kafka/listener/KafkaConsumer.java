ac11a264cc4a7c341c9181db5583b74dc118f435
soumyadip007 <soumyadip.note@gmail.com>
2020-03-21 00:30:23 +0530

    Controller rl

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/listener/KafkaConsumer.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/listener/KafkaConsumer.java
@@ -1,23 +1,23 @@
-//package com.microservice.architecture.kafka.listener;
-//
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Service;
-//
-//import com.microservice.architecture.entity.MS;
-//
-//@Service
-//public class KafkaConsumer {
-//
-//	
-//	@KafkaListener(topics="Kafka1",groupId="string")
-//	public void consumeString(String msg)
-//	{
-//		System.out.println("Consumed message "+msg);
-//	}
-//	
-//	@KafkaListener(topics="Kafka2",groupId="json")
-//	public void consumeMS(MS ms)
-//	{
-//		System.out.println("Consumed message "+ms);
-//	}
-//}
+package com.microservice.architecture.kafka.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.microservice.architecture.entity.MS;
+
+@Service
+public class KafkaConsumer {
+
+	
+	@KafkaListener(topics="Kafka1",groupId="string")
+	public void consumeString(String msg)
+	{
+		System.out.println("Consumed message "+msg);
+	}
+	
+	@KafkaListener(topics="Kafka2",groupId="json")
+	public void consumeMS(MS ms)
+	{
+		System.out.println("Consumed message "+ms);
+	}
+}
