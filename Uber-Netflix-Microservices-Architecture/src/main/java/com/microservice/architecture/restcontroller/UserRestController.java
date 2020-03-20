5b5405813871f4657040ac8356120ff95b7ebcbc
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 21:05:15 +0530

    Rest Controller MS

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/restcontroller/UserRestController.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/restcontroller/UserRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.microservice.architecture.entity.Microservices;
+import com.microservice.architecture.entity.MS;
 import com.microservice.architecture.service.MicroserviceService;
 
 @CrossOrigin(origins ="*",allowedHeaders="*",maxAge=200000) 
@@ -30,33 +30,33 @@ public class UserRestController {
 	
 
 	@GetMapping
-	public List<Microservices> FindAll() {
+	public List<MS> FindAll() {
 	
 		return microservice.findAll();
 	}
 	
 	@GetMapping("/{pageNo}")
-	public List<Microservices> FindPage(@PathVariable int pageNo) {
+	public List<MS> FindPage(@PathVariable int pageNo) {
 	
-		List<Microservices> list = microservice.findPage(pageNo,100, "id");
+		List<MS> list = microservice.findPage(pageNo,100, "id");
 		
 		return list;
 	}
 	
 	@GetMapping("/{pageNo}/{pageSize}")
-	public List<Microservices> FindPageWithSize(@PathVariable int pageNo,
+	public List<MS> FindPageWithSize(@PathVariable int pageNo,
 								@PathVariable int pageSize) {
 	
-		List<Microservices> list = microservice.findPage(pageNo,pageSize, "id");
+		List<MS> list = microservice.findPage(pageNo,pageSize, "id");
 		
 		return list;
 	}
 	
 	@GetMapping("/{pageNo}/sort/{sort}")
-	public List<Microservices> FindPageWithSorting(@PathVariable int pageNo,
+	public List<MS> FindPageWithSorting(@PathVariable int pageNo,
 								@PathVariable String sort) {
 	
-		List<Microservices> list = microservice.findPage(pageNo,100, sort);
+		List<MS> list = microservice.findPage(pageNo,100, sort);
 		
 		return list;
 	}
@@ -64,42 +64,42 @@ public class UserRestController {
 	
 	
 	@GetMapping("/{pageNo}/{pageSize}/{sort}")
-	public List<Microservices> FindPageWithLimitAndSorting(@PathVariable int pageNo,
+	public List<MS> FindPageWithLimitAndSorting(@PathVariable int pageNo,
 								@PathVariable int pageSize,
 								@PathVariable String sort) {
 	
-		List<Microservices> list = microservice.findPage(pageNo,pageSize,sort);
+		List<MS> list = microservice.findPage(pageNo,pageSize,sort);
 		
 		return list;
 	}
 	
 	@GetMapping("/limit/{pageSize}")
-	public List<Microservices> FindLimit(@PathVariable int pageSize) {
+	public List<MS> FindLimit(@PathVariable int pageSize) {
 	
-		List<Microservices> list = microservice.findPage(0,pageSize,"id");
+		List<MS> list = microservice.findPage(0,pageSize,"id");
 		
 		return list;
 	}
 	
 
 	@GetMapping("/limit/{pageSize}/{sort}")
-	public List<Microservices> FindLimitWithSort(@PathVariable int pageSize,
+	public List<MS> FindLimitWithSort(@PathVariable int pageSize,
 									@PathVariable String sort) {
 	
-		List<Microservices> list = microservice.findPage(0,pageSize,sort);
+		List<MS> list = microservice.findPage(0,pageSize,sort);
 		
 		return list;
 	}
 	
 	
 	@GetMapping("/id/{theId}")
-	public Microservices findById(@PathVariable int theId) {
+	public MS findById(@PathVariable int theId) {
 		
 		return microservice.findById(theId);
 	}
 	
 	@GetMapping("/microentity1/{theId}")
-	public List<Microservices> findByMicroentity1(@RequestHeader("key") String language,
+	public List<MS> findByMicroentity1(@RequestHeader("key") String language,
 			@PathVariable String theId) {
 		System.out.println(language);
 		System.out.println(language);
