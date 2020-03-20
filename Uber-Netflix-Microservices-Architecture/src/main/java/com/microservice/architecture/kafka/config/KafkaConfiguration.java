d20d67fd4063b308dab08a7f28f87a7e4c88c219
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 22:27:26 +0530

    consumerUser

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/config/KafkaConfiguration.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/config/KafkaConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import com.microservice.architecture.entity.MS;
 
 @EnableKafka
 @Configuration
@@ -19,6 +20,20 @@ public class KafkaConfiguration {
 	
 	@Bean
 	public  ConsumerFactory<String,String> consumerFactory(){
+		
+		
+		Map<String, Object> config=new HashMap<>();
+		
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"120.0.0.1");
+		config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_id");
+		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class);
+		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,"120.0.0.1");
+		
+		return new DefaultKafkaConsumerFactory<>(config);
+	}
+	
+	@Bean
+	public  ConsumerFactory<String,MS> consumerUser(){
 		
 		
 		Map<String, Object> config=new HashMap<>();
