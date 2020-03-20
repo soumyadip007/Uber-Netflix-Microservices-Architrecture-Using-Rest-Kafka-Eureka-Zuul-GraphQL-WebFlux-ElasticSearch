08d669c2e3cefb5fb06c3db725b6b694f4fe6e24
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 21:05:45 +0530

    MS Page rm

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/repository/MicroservicesPageRepository.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/repository/MicroservicesPageRepository.java
@@ -3,9 +3,9 @@ package com.microservice.architecture.repository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import com.microservice.architecture.entity.Microservices;
+import com.microservice.architecture.entity.MS;
 
 @Repository
-public interface MicroservicesPageRepository extends PagingAndSortingRepository<Microservices, Integer> {
+public interface MicroservicesPageRepository extends PagingAndSortingRepository<MS, Integer> {
 
 }
