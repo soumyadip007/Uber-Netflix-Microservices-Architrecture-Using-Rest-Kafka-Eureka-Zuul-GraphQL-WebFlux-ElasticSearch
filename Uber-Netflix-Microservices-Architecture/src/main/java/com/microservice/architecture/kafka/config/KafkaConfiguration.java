f3bc3c70038368f62c65cdf465c7a38594f3a3ce
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 22:20:22 +0530

    @EnableKafka @Configuration

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/config/KafkaConfiguration.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/config/KafkaConfiguration.java
@@ -1,5 +1,10 @@
 package com.microservice.architecture.kafka.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@Configuration
 public class KafkaConfiguration {
 
 }
