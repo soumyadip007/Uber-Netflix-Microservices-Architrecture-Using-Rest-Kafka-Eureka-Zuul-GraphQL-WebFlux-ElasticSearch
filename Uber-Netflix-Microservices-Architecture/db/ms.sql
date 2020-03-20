aa867079ebd6f6f330b4ed1be94b836cc4acbbc3
soumyadip007 <soumyadip.note@gmail.com>
2020-03-21 00:50:24 +0530

    ms.sql added

---
+++ Uber-Netflix-Microservices-Architecture/db/ms.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 20, 2020 at 08:18 PM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.1.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ms`
+--
+
+CREATE TABLE `ms` (
+  `id` int(11) NOT NULL,
+  `a` varchar(255) DEFAULT NULL,
+  `b` varchar(255) DEFAULT NULL,
+  `c` varchar(255) DEFAULT NULL,
+  `d` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ms`
+--
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ms`
+--
+ALTER TABLE `ms`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
