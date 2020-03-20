bb42d722075f7dc7e221cd3f849513d84c1e3897
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 22:22:52 +0530

    MAP Cnonfgi

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/config/KafkaConfiguration.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/config/KafkaConfiguration.java
@@ -1,5 +1,8 @@
 package com.microservice.architecture.kafka.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -12,6 +15,10 @@ public class KafkaConfiguration {
 	
 	@Bean
 	public  ConsumerFactory<String,String> consumerFactory(){
+		
+		
+		Map<String, Object> config=new HashMap<>();
+		
 		
 	}
 }
