a3ee209d5c1a15d5ba766b6121f59e538d712935
soumyadip007 <soumyadip.note@gmail.com>
2020-03-21 00:13:00 +0530

    KafkaConsumer

---
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/restcontroller/KafkaController.java
@@ -0,0 +1,10 @@
+package com.microservice.architecture.restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Kafka")
+public class KafkaController {
+
+}
