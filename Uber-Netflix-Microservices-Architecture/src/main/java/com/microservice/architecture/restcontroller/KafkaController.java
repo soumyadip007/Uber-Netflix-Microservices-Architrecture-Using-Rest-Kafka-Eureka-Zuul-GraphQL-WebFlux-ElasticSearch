74752faa1b0ba12e44809dbb18880a0c4d87cf4b
soumyadip007 <soumyadip.note@gmail.com>
2020-03-21 00:15:30 +0530

    Kafkacontroller RM

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/restcontroller/KafkaController.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/restcontroller/KafkaController.java
@@ -1,28 +1,28 @@
-package com.microservice.architecture.restcontroller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("Kafka")
-public class KafkaController {
-
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
-
-    private static final String TOPIC = "Kafka1";
-
-
-    @GetMapping
-    public String kafka1() {
-
-        kafkaTemplate.send(TOPIC, "Kafka");
-
-        return "Published successfully";
-    }
-	
-}
+//package com.microservice.architecture.restcontroller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("Kafka")
+//public class KafkaController {
+//
+//
+//    @Autowired
+//    private KafkaTemplate<String, String> kafkaTemplate;
+//
+//    private static final String TOPIC = "Kafka1";
+//
+//
+//    @GetMapping
+//    public String kafka1() {
+//
+//        kafkaTemplate.send(TOPIC, "Kafka");
+//
+//        return "Published successfully";
+//    }
+//	
+//}
