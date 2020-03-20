ac0f8926e63423ef1f88e57c7e56a327b5ac78c1
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 21:06:31 +0530

    Interface config

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/service/MicroserviceServiceInterface.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/service/MicroserviceServiceInterface.java
@@ -2,17 +2,17 @@ package com.microservice.architecture.service;
 
 import java.util.List;
 
-import com.microservice.architecture.entity.Microservices;
+import com.microservice.architecture.entity.MS;
 
 public interface MicroserviceServiceInterface {
 
-	public List<Microservices> findAll();
+	public List<MS> findAll();
 
-	public void save(Microservices OBJ);
+	public void save(MS OBJ);
 	
 	public void deleteById(int theId);
 	
-	public Microservices findById(int theId);
+	public MS findById(int theId);
 
 	//public List<Microservices> findByMicroentity1(String seq);
 }
