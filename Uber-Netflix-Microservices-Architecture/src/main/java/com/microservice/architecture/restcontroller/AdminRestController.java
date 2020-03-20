48ee16f72b5e53545601645c77ec7fcb16480673
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 21:06:09 +0530

    Admin rest mx

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/restcontroller/AdminRestController.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/restcontroller/AdminRestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.microservice.architecture.entity.Microservices;
+import com.microservice.architecture.entity.MS;
 import com.microservice.architecture.service.MicroserviceService;
 
 @CrossOrigin(origins ="*",allowedHeaders="*",maxAge=200000) 
@@ -26,67 +26,67 @@ public class AdminRestController {
 	private MicroserviceService microService;
 
 	@GetMapping
-	public List<Microservices> FindAll() {
+	public List<MS> FindAll() {
 	
 		return microService.findAll();
 	}
 	
 
 	@GetMapping("/{pageNo}")
-	public List<Microservices> FindPage(@PathVariable int pageNo) {
+	public List<MS> FindPage(@PathVariable int pageNo) {
 	
-		List<Microservices> list = microService.findPage(pageNo,100, "id");
+		List<MS> list = microService.findPage(pageNo,100, "id");
 		
 		return list;
 	}
 	
 	@GetMapping("/{pageNo}/{pageSize}")
-	public List<Microservices> FindPageWithSize(@PathVariable int pageNo,
+	public List<MS> FindPageWithSize(@PathVariable int pageNo,
 								@PathVariable int pageSize) {
 	
-		List<Microservices> list = microService.findPage(pageNo,pageSize, "id");
+		List<MS> list = microService.findPage(pageNo,pageSize, "id");
 		
 		return list;
 	}
 	
 	@GetMapping("/{pageNo}/{pageSize}/{sort}")
-	public List<Microservices> FindPageWithSorting(@PathVariable int pageNo,
+	public List<MS> FindPageWithSorting(@PathVariable int pageNo,
 								@PathVariable int pageSize,
 								@PathVariable String sort) {
 	
-		List<Microservices> list = microService.findPage(pageNo,pageSize,sort);
+		List<MS> list = microService.findPage(pageNo,pageSize,sort);
 		
 		return list;
 	}
 	
 	@GetMapping("/limit/{pageSize}")
-	public List<Microservices> FindLimit(@PathVariable int pageSize) {
+	public List<MS> FindLimit(@PathVariable int pageSize) {
 	
-		List<Microservices> list = microService.findPage(0,pageSize,"id");
+		List<MS> list = microService.findPage(0,pageSize,"id");
 		
 		return list;
 	}
 	
 
 	@GetMapping("/limit/{pageSize}/{sort}")
-	public List<Microservices> FindLimitWithSort(@PathVariable int pageSize,
+	public List<MS> FindLimitWithSort(@PathVariable int pageSize,
 									@PathVariable String sort) {
 	
-		List<Microservices> list = microService.findPage(0,pageSize,sort);
+		List<MS> list = microService.findPage(0,pageSize,sort);
 		
 		return list;
 	}
 	
 
 	@GetMapping("/id/{theId}")
-	public Microservices findById(@PathVariable int theId) {
+	public MS findById(@PathVariable int theId) {
 		
 		return microService.findById(theId);
 	}
 	
 
 	@PostMapping("/save")
-	public Microservices add(@RequestBody Microservices obj) {
+	public MS add(@RequestBody MS obj) {
 		
 		obj.setId(0);
 		
@@ -96,7 +96,7 @@ public class AdminRestController {
 	}
 	
 	@PostMapping("/update")
-	public Microservices update(@RequestBody Microservices obj) {
+	public MS update(@RequestBody MS obj) {
 		
 		microService.save(obj);
 		
