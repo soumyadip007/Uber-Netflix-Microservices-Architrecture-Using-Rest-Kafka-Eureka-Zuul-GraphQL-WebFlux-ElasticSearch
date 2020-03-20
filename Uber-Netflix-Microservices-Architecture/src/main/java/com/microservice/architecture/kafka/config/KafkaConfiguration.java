6b5c2ce509ee11e9820c4a86002aca3122b6b0cb
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 22:24:00 +0530

    Kafka Server Config

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/config/KafkaConfiguration.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/config/KafkaConfiguration.java
@@ -3,6 +3,7 @@ package com.microservice.architecture.kafka.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -18,6 +19,11 @@ public class KafkaConfiguration {
 		
 		
 		Map<String, Object> config=new HashMap<>();
+		
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"120.0.0.1");
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"120.0.0.1");
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"120.0.0.1");
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"120.0.0.1");
 		
 		
 	}
