a880f95ae14ffdf70e308139d50b72c5d919c614
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 22:21:31 +0530

    ConsumerFactory bean added

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/config/KafkaConfiguration.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/kafka/config/KafkaConfiguration.java
@@ -1,10 +1,17 @@
 package com.microservice.architecture.kafka.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
 
 @EnableKafka
 @Configuration
 public class KafkaConfiguration {
 
+	
+	@Bean
+	public  ConsumerFactory<String,String> consumerFactory(){
+		
+	}
 }
