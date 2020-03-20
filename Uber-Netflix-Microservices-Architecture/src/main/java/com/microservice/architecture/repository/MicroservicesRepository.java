eac5e8f68b97ea3bb38be7ee2975d84bd9be5195
soumyadip007 <soumyadip.note@gmail.com>
2020-03-20 16:59:16 +0530

    MicroservicesRepository Config

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/repository/MicroservicesRepository.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/repository/MicroservicesRepository.java
@@ -8,16 +8,10 @@ import org.springframework.stereotype.Repository;
 import com.microservice.architecture.entity.Microservices;
 
 
-@Repository("GA4GHRepository")
+@Repository("MicroservicesRepository")
 public interface MicroservicesRepository extends JpaRepository<Microservices, Integer> {
 
 	public Microservices findById(int id);
-	
-	public List<Microservices> findBySequencename(String seq);
-	
-	public List<Microservices> findByGenbankaccn(String seq);
-	
-	public List<Microservices> findByRefseqaccn(String seq);
-	
-	public List<Microservices> findBySequencelength(String seq);
+
+	public List<Microservices> findByMicroentity1(String seq);
 }
