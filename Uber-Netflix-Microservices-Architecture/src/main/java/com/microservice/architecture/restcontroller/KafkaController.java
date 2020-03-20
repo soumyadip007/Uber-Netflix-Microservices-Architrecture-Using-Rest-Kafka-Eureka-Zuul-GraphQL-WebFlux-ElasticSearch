b87b0746c6f252ff534a6656f87430dcbac08e51
soumyadip007 <soumyadip.note@gmail.com>
2020-03-21 00:14:48 +0530

    kafka1() added

--- Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/restcontroller/KafkaController.java
+++ Uber-Netflix-Microservices-Architecture/src/main/java/com/microservice/architecture/restcontroller/KafkaController.java
@@ -1,5 +1,8 @@
 package com.microservice.architecture.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("Kafka")
 public class KafkaController {
 
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private static final String TOPIC = "Kafka1";
+
+
+    @GetMapping
+    public String kafka1() {
+
+        kafkaTemplate.send(TOPIC, "Kafka");
+
+        return "Published successfully";
+    }
+	
 }
